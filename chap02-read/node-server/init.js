2f5877857f3653175223e7dd0e98359bbc2ee01a
Katsuya Endoh <studio@enkatsu.org>
2024-06-23 07:20:25 +0900

    format

--- chap02-read/node-server/init.js
+++ chap02-read/node-server/init.js
@@ -6,7 +6,7 @@ db.serialize(() => {
     db.run('create table if not exists balls(id integer primary key autoincrement, x real, y real, r real)')
     for (let i = 0; i < 3; i++) {
         db.run('insert into balls(x, y, r) values(?, ?, ?)', Math.random(), Math.random(), Math.random())
-    }    
+    }
     db.all('select * from balls', (err, row) => {
         console.log(row)
     })
