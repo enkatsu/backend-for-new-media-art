c6fffc3a762618b1d73a546801619aa0f4ff8b99
Katsuya Endoh <studio@enkatsu.org>
2024-06-25 21:24:24 +0900

    update db:init

--- chap03-create/node-server/init.js
+++ chap03-create/node-server/init.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database('./db.sqlite')
 db.serialize(() => {
     db.run('drop table if exists balls')
     db.run('create table if not exists balls(id integer primary key autoincrement, x real, y real, r real)')
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 50; i++) {
         db.run('insert into balls(x, y, r) values(?, ?, ?)', Math.random(), Math.random(), Math.random())
     }
     db.all('select * from balls', (err, row) => {
